27d4a900850c99df6d42f89d4a2f20c6c7d21f60
Christof Bachmann <code@christof-b.de>
2018-09-08 04:05:38 +0200

    cleanup boilerplate sample code

--- src/app.js
+++ src/app.js
@@ -7,34 +7,14 @@ import "./helpers/external_links.js";
 // Bootstrap
 import "../node_modules/bootstrap/dist/js/bootstrap.bundle.js"
 import "../node_modules/bootstrap/dist/css/bootstrap.css"
+
 import jquery from "jquery";
 
-// ----------------------------------------------------------------------------
-// Everything below is just to show you how it works. You can delete all of it.
-// ----------------------------------------------------------------------------
-
 import {
-  remote,
-  ipcRenderer
+  remote
 } from "electron";
-import jetpack from "fs-jetpack";
-import {
-  greet
-} from "./hello_world/hello_world";
-import env from "env";
 
-const app = remote.app;
-const appDir = jetpack.cwd(app.getAppPath());
-
-// Holy crap! This is browser window with HTML and stuff, but I can read
-// files from disk like it's node.js! Welcome to Electron world :)
-const manifest = appDir.read("package.json", "json");
-
-const osMap = {
-  win32: "Windows",
-  darwin: "macOS",
-  linux: "Linux"
-};
+const dialog = remote.dialog;
 
 jquery('#start-button').click(function(ev) {
   ev.preventDefault();
@@ -51,11 +31,8 @@ jquery('#inputChangeCommand').click(function() {
   }
 });
 
-const dialog = remote.dialog;
-
 jquery('#run').click(function(ev) {
   ev.preventDefault();
-  //ipcRenderer.send('open-error-dialog');
   dialog.showErrorBox('Error', "Sorry, not implemented yet.")
 })
 
