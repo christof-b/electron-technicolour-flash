9d695a1fd6197e425ccc3d214ac79ff97d355558
Christof Bachmann <code@christof-b.de>
2018-09-16 13:27:27 +0200

    Moved code for gui interaction in own class

--- src/app.js
+++ src/app.js
@@ -7,8 +7,9 @@ import "./helpers/external_links.js";
 // Bootstrap
 import "../node_modules/bootstrap/dist/js/bootstrap.bundle.js"
 import "../node_modules/bootstrap/dist/css/bootstrap.css"
-
+import GUI from "./technicolour/GUI"
 import jquery from "jquery";
+import got from "got";
 
 import {
   remote
@@ -16,12 +17,6 @@ import {
 
 const dialog = remote.dialog;
 const fs = remote.require('fs');
-
-import got from "got";
-import FormData from "form-data";
-import {
-  CookieJar
-} from 'tough-cookie';
 
 import path from 'path';
 import env from "env";
@@ -41,125 +36,75 @@ jquery('#inputChangeCommand').click(function() {
   }
 });
 
-jquery('#run').click(function(ev) {
+jquery('#run').click(async (ev) => {
   ev.preventDefault();
   jquery('#form').hide();
-  const username = jquery('#inputUsername').val();
-  const password = jquery('#inputPassword').val();
-  const host = jquery('#inputTargetIP').val();
-  const webview = document.querySelector('webview');
-  let tries = 0;
-  webview.addEventListener('did-finish-load', (event) => {
-
-    if (/login/.test(webview.getURL())) {
-      if (tries === 0) {
-        tries++;
-        fs.readFile(path.join(__dirname, '/webview/login.js'), 'utf8', (err, data) => {
-          if (err) throw err;
-          data = data.replace(/##username##/gi, username);
-          data = data.replace(/##password##/gi, password);
-          webview.executeJavaScript(data);
-          jquery('#progress').append('<p class="text-info">Authenticating...</p>');
-        });
-      } else {
-        jquery('#progress').append('<p class="text-danger">Auth failed!</p>');
-      }
-    } else {
-      jquery('#progress').append('<p class="text-success">Auth successful</p>');
-      webview.send('csrf-token-request');
-    }
-  });
-
-  webview.addEventListener("ipc-message", function(e) {
-    if (e.channel === "csrf-token-response") {
-      const token = e.args[0];
-      const firmware = jquery('#inputFirmwareFileName').val();
-      const firmwarePath = path.join(__dirname, '/firmware/' + firmware);
-
-      const stats = fs.statSync(firmwarePath);
-      console.log(stats);
-
-      fs.readFile(firmwarePath, (err, data) => {
-
-        const form = new FormData({
-          maxDataSize: stats.size + 1024
-        });
-        form.append('CSRFtoken', token);
-        form.append('upgradefile', new Buffer(data, 'binary'), {
-          knownLength: stats.size
-        });
-        const cookieJar = new CookieJar();
-        let session = webview.getWebContents().session;
-
-        session.cookies.get({
-          url: 'http://' + host
-        }, function(error, cookies) {
-          for (var i = 0; i < cookies.length; i++) {
-            let info = cookies[i];
-            cookieJar.setCookie(`${info.name}=${info.value};`, 'http://' + host, () => {});
-            console.log(info.name, info.value);
-          }
-
-          jquery('#progress').append('<p class="text-info">Uploading firmware...</p>');
-
-          (async () => {
-            try {
-              jquery('#back').attr('disabled', 'disabled');
-              const response = await got.post('/modals/gateway-modal.lp?action=upgradefw', {
-                cookieJar: cookieJar,
-                baseUrl: 'http://' + host,
-                body: form,
-                agent: null
-              }).on('uploadProgress', (progress) => {
-                console.log(progress);
-              });
-              jquery('#back').removeAttr('disabled');
-              const result = JSON.parse(response.body);
-              if (result.success !== undefined && result.success) {
-                jquery('#progress').append('<p class="text-success">Upload successful!</p>');
-                jquery('#progress').append('<p class="text-info">Waiting for reboot...</p>');
-                setTimeout(function() {
-                  jquery('#progress').append('<p class="text-info">Try to reconnect...</p>');
-                  (async () => {
-                    await got('http://' + host, {
-                      retry: {
-                        retries: (retry, error) => {
-                          jquery('#progress').append('<p class="text-mute">Retry #' + retry + ', waiting...</p>')
-                          return 5000;
-                        }
-                      }
-                    });
-                    jquery('#progress').append('<p class="text-success">Ready to continue!</p>');
-                    //TODO continue
-                    jquery('#back').html('back');
-                  })();
-                }, 120000);
-              } else {
-                jquery('#back').html('back');
-                jquery('#progress').append('<p class="text-danger">Upload failed!</p>');
-                console.log(response.body);
-                console.log(result);
-              }
-            } catch (error) {
-              jquery('#back').html('back');
-              jquery('#back').removeAttr('disabled');
-              jquery('#progress').append('<p class="text-danger">An error occured!</p>');
-              console.log(error);
-              //=> 'Internal server error ...'
-            }
-          })();
-
-        });
-      });
-    }
-  });
-
-
-  jquery('#app').append('<div class="jumbotron mt-4" id="progress"><h1>Progress</h1><p class="text-info">Loading firmware...</p></div><button type="button" class="btn btn-primary" id="back">cancel</button>');
+  jquery('#app').append('<div class="jumbotron mt-4" id="progress"><h1>Progress</h1></div><button type="button" class="btn btn-primary" id="back">cancel</button>');
   jquery('#back').click(() => {
     location.reload();
   });
-  webview.loadURL('http://' + host + '/login.lp');
+
+  const username = jquery('#inputUsername').val();
+  const password = jquery('#inputPassword').val();
+  const host = jquery('#inputTargetIP').val();
+
+  const gui = new GUI(document.querySelector('#client'), host);
+  console.log(gui);
+
+  jquery('#progress').append('<p class="text-info">Authenticating...</p>');
+  const loginResult = await gui.login(username, password);
+  if (loginResult !== true) {
+    jquery('#progress').append('<p class="text-danger">Auth failed!</p>');
+  } else {
+    jquery('#progress').append('<p class="text-success">Auth successful</p>');
+
+    jquery('#progress').append('<p class="text-info">Loading firmware...</p>');
+
+    const firmware = jquery('#inputFirmwareFileName').val();
+    const firmwarePath = path.join(__dirname, '/firmware/' + firmware);
+
+    jquery('#progress').append('<p class="text-info">Uploading firmware...</p>');
+
+    try {
+      jquery('#back').attr('disabled', 'disabled');
+      const result = await gui.uploadFirmware(firmwarePath);
+      jquery('#back').removeAttr('disabled');
+
+      if (result.success !== undefined && result.success) {
+        jquery('#progress').append('<p class="text-success">Upload successful!</p>');
+        jquery('#progress').append('<p class="text-info">Waiting for reboot...</p>');
+        setTimeout(function() {
+          jquery('#progress').append('<p class="text-info">Try to reconnect...</p>');
+          (async () => {
+            await got('http://' + host, {
+              retry: {
+                retries: (retry, error) => {
+                  jquery('#progress').append('<p class="text-mute">Retry #' + retry + ', waiting...</p>')
+                  return 5000;
+                }
+              }
+            });
+            jquery('#progress').append('<p class="text-success">Ready to continue!</p>');
+            //TODO continue
+            jquery('#back').html('back');
+          })();
+        }, 120000);
+      } else {
+        jquery('#back').html('back');
+        jquery('#progress').append('<p class="text-danger">Upload failed!</p>');
+        console.log(response.body);
+        console.log(result);
+      }
+    } catch (error) {
+      jquery('#back').html('back');
+      jquery('#back').removeAttr('disabled');
+      jquery('#progress').append('<p class="text-danger">An error occured!</p>');
+      console.log(error);
+      //=> 'Internal server error ...'
+    }
+
+  }
+
 });
 
 if (env.name === "development") {
---
+++ src/technicolour/GUI.js
@@ -0,0 +1,115 @@
+import got from "got";
+import FormData from "form-data";
+import {
+  CookieJar
+} from 'tough-cookie';
+import {
+  remote
+} from "electron";
+const fs = remote.require('fs');
+import path from 'path';
+import env from "env";
+
+const guiClass = class GUI {
+
+  constructor(webview, host) {
+    this.webview = webview;
+    this.host = host;
+    this.isAuthenticated = false;
+
+  }
+
+  login(username, password) {
+    const webview = this.webview;
+    const host = this.host;
+    return new Promise(resolve => {
+      let tries = 0;
+      webview.addEventListener('did-finish-load', (event) => {
+        if (/login/.test(webview.getURL())) {
+          if (tries === 0) {
+            tries++;
+            fs.readFile(path.join(__dirname, '/webview/login.js'), 'utf8', (err, data) => {
+              if (err) throw err;
+              data = data.replace(/##username##/gi, username);
+              data = data.replace(/##password##/gi, password);
+              webview.executeJavaScript(data);
+            });
+          } else {
+            resolve(false);
+          }
+        } else {
+          this.isAuthenticated = true;
+          resolve(true);
+        }
+      });
+      webview.loadURL('http://' + host + '/login.lp');
+    });
+  }
+
+  getCSRFtoken() {
+    const webview = this.webview;
+    const host = this.host;
+    return new Promise(resolve => {
+      webview.addEventListener("ipc-message", function(e) {
+        if (e.channel === "csrf-token-response") {
+          resolve(e.args[0]);
+        }
+      });
+      webview.send('csrf-token-request');
+    });
+  }
+
+  uploadFirmware(firmwarePath) {
+    if (!this.isAuthenticated) {
+      throw new Error('Please login before upload!');
+    }
+    const webview = this.webview;
+    const host = this.host;
+    return new Promise(resolve => {
+      const stats = fs.statSync(firmwarePath);
+      console.log(stats);
+
+      fs.readFile(firmwarePath, async (err, data) => {
+
+        const token = await this.getCSRFtoken();
+        const form = new FormData({
+          maxDataSize: stats.size + 1024
+        });
+        form.append('CSRFtoken', token);
+        form.append('upgradefile', new Buffer(data, 'binary'), {
+          knownLength: stats.size
+        });
+        const cookieJar = new CookieJar();
+        let session = webview.getWebContents().session;
+
+        session.cookies.get({
+          url: 'http://' + host
+        }, function(error, cookies) {
+          for (var i = 0; i < cookies.length; i++) {
+            let info = cookies[i];
+            cookieJar.setCookie(`${info.name}=${info.value};`, 'http://' + host, () => {});
+            console.log(info.name, info.value);
+          }
+
+          (async () => {
+
+            const response = await got.post('/modals/gateway-modal.lp?action=upgradefw', {
+              cookieJar: cookieJar,
+              baseUrl: 'http://' + host,
+              body: form,
+              agent: null
+            }).on('uploadProgress', (progress) => {
+              console.log(progress);
+            });
+            resolve(JSON.parse(response.body));
+
+          })();
+
+        });
+      });
+    });
+  }
+
+}
+
+export default guiClass;
