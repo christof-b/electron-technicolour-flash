958a46ffea49c62e755a247d1b508f4c41ae4cb3
Christof Bachmann <code@christof-b.de>
2018-09-17 01:48:55 +0200

    Implemented ssh cmd exec to finish rooting

--- src/app.js
+++ src/app.js
@@ -15,6 +15,7 @@ import {
 import jquery from "jquery";
 import got from "got";
 import sleep from "./helpers/sleep";
+import SSHClient from "node-ssh";
 
 import {
   remote
@@ -66,6 +67,25 @@ function doLogin(gui) {
   });
 }
 
+function rebooting(gui, host) {
+  return new Promise(async (resolve, reject) => {
+    jquery('#progress').append('<p class="text-info">Waiting for reboot...</p>');
+    await sleep(120000);
+    jquery('#progress').append('<p class="text-info">Try to reconnect...</p>');
+    await got('http://' + host, {
+      retry: {
+        retries: (retry, error) => {
+          jquery('#progress').append('<p class="text-mute">Retry #' + retry + ', waiting...</p>')
+          return 5000;
+        }
+      }
+    });
+
+    resolve(await doLogin(gui));
+
+  });
+}
+
 jquery('#run').click(async (ev) => {
   ev.preventDefault();
   jquery('#form').hide();
@@ -98,25 +118,13 @@ jquery('#run').click(async (ev) => {
         if (result.success !== undefined && result.success) {
           jquery('#progress').append('<p class="text-success">Upload successful!</p>');
 
-          jquery('#progress').append('<p class="text-info">Waiting for reboot...</p>');
-          await sleep(120000);
-          jquery('#progress').append('<p class="text-info">Try to reconnect...</p>');
-          await got('http://' + host, {
-            retry: {
-              retries: (retry, error) => {
-                jquery('#progress').append('<p class="text-mute">Retry #' + retry + ', waiting...</p>')
-                return 5000;
-              }
-            }
-          });
-
-          authenticated = await doLogin(gui);
+          authenticated = await rebooting(gui, host);
           if (authenticated) {
             jquery('#progress').append('<p class="text-success">Flash successfull!</p>');
           } else {
             jquery('#back').html('back');
             jquery('#progress').append('<p class="text-danger">Failed to reauth!</p>');
-            throw new Error('Failed to reauth');
+            throw Error('Failed to reauth');
           }
 
         } else {
@@ -132,28 +140,110 @@ jquery('#run').click(async (ev) => {
       if (jquery('#inputRootDevice').is(':checked')) {
         jquery('#progress').append('<p class="text-info">Start rooting of device...</p>');
         const commands = jquery('#inputSplitCommand').is(':checked') ? jquery('#inputCommand').val().split(';') : [jquery('#inputCommand').val()];
-        console.log(commands);
+        const method = jquery('#inputRootMethod').val();
         for (let i = 0; i < commands.length; i++) {
           const cmd = commands[i];
-          jquery('#progress').append('<p class="text-mute">Exec "' + cmd + '"...</p>');
-          const result = await gui.exexCMD(cmd, jquery('#inputRootMethod').val());
+          jquery('#progress').append('<p class="text-mute">' + method + ' Exec "' + cmd + '"...</p>');
+          const result = await gui.exexCMD(cmd, method);
           if (result !== true) {
             throw new Error('Failed to exec: ' + cmd);
           }
           await sleep(5000);
         }
 
+        console.log('Establish SSH connection...');
+        const ssh = new SSHClient();
+        await ssh.connect({
+          host: host,
+          username: 'root',
+          password: 'root'
+        });
+
+        const handler = require('serve-handler');
+        const http = require('http');
+        const portfinder = require('portfinder');
+        const os = require("os");
+
+        portfinder.basePort = 8080;
+        const localPort = await new Promise((resolve, reject) => {
+          portfinder.getPort(function(err, port) {
+            if (err) {
+              reject(err);
+            }
+            resolve(port);
+          });
+        });
+
+        const remoteIP = host.split('.');
+        let localhost = '0.0.0.0';
+
+        Object.values(os.networkInterfaces()).forEach((iface) => {
+          iface.forEach((config) => {
+            const localIP = config.address.split('.');
+            if (localIP[0] == remoteIP[0] && localIP[1] == remoteIP[1] && localIP[2] == remoteIP[2]) {
+              localhost = config.address;
+            }
+          })
+        })
+
+        const server = http.createServer((request, response) => {
+          // You pass two more arguments for config and middleware
+          // More details here: https://github.com/zeit/serve-handler#options
+          return handler(request, response, {
+            "public": __dirname
+          });
+        })
+
+        await new Promise((resolve, reject) => {
+          server.listen(localPort, localhost, () => {
+            console.log('Serving at http://' + localhost + ':' + localPort);
+            resolve(true);
+          }).on('error', (e) => {
+            server.close();
+            reject(e);
+          });
+        });
+
+        const sshCommands = [
+          'wget -O /tmp/dga4132_unlocked.tar.gz http://' + localhost + ':' +
+          localPort + '/gui/dga4132_unlocked.tar.gz',
+          'tar -zxvf /tmp/dga4132_unlocked.tar.gz -C /',
+          '/etc/init.d/rootdevice force',
+          '/etc/init.d/rootdevice force'
+        ];
+
+        for (let c = 0; c < sshCommands.length; c++) {
+          jquery('#progress').append('<p class="text-mute">SSH Exec "' + sshCommands[c] + '"...</p>');
+          const result = await ssh.execCommand(sshCommands[c], {}, (err, channel) => {
+            console.log(err);
+            throw Error(err);
+          });
+          if (result.code !== 0) {
+            console.log(result);
+            throw Error(result);
+          }
+        };
+
+        server.close();
+
+        ssh.execCommand('reboot', {}, (err, channel) => {
+          console.log(err);
+        });
+
+        /*
+        //TODO fix gui or ngynx or chrome
+        authenticated = await rebooting(gui, host);
+        if (authenticated) {
+          jquery('#progress').append('<p class="text-success">Rooting successfull!</p>');
+        } else {
+          jquery('#back').html('back');
+          jquery('#progress').append('<p class="text-danger">Failed to reauth!</p>');
+          throw Error('Failed to reauth');
+        }
+        */
+        jquery('#progress').append('<p class="text-success">After reboot you can login via web and ssh (root:root)</p>');
       }
-
-      //TODO scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null dga4132_unlocked.tar.gz root@192.168.1.1:/tmp
-      //TODO ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null root@192.168.1.1
-      //TODO - tar -zxvf /tmp/dga4132_unlocked.tar.gz -C /
-      //TODO - /etc/init.d/rootdevice force
-      //TODO - /etc/init.d/rootdevice force
-      //TODO - reboot
-
       jquery('#progress').append('<p class="text-success">Done!</p>');
-
       jquery('#back').html('back');
     }
 
